da24693c143141c6491c84a81968c6ff8bef0f05
Udara <udara@ubuntu.ubuntu-domain>
2014-07-07 13:01:42 +0530

    create 2 jars to check jar hell problem

---
+++ zoo/watcher/src/main/java/com/zoo/watcher/Watcher.java
@@ -0,0 +1,11 @@
+package com.zoo.watcher;
+
+/**
+ * Created by udara on 7/7/14.
+ */
+public class Watcher {
+
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
---
+++ zoo/zootwo/src/main/java/com/zoo/Animal.java
@@ -0,0 +1,7 @@
+package com.zoo;
+
+/**
+ * Created by udara on 7/7/14.
+ */
+public class Animal {
+}
